b5fe0455f9daa31214721ad90fa07a9331279ce4
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-10-26 12:44:15 +0300

    2nd commit

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, render_template, request, redirect, url_for
+from flask import Flask, make_response, render_template
 from flask_migrate import Migrate
-
-from models import db, Hero, Power  # Import the Power model
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
@@ -13,28 +13,19 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.route('/')
+def home():
+    return 'Welcome to the Heroes and Powers App'
+
+@app.route('/heroes')
+def list_heroes():
+    heroes = Hero.query.all()
+    return render_template('heroes.html', heroes=heroes)
+
+@app.route('/powers')
+def list_powers():
+    powers = Power.query.all()
+    return render_template('powers.html', powers=powers)
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Create the database tables
-
-    @app.route('/')
-    def index():
-        heroes = Hero.query.all()
-        return render_template('index.html', heroes=heroes)
-
-    @app.route('/powers')
-    def list_powers():
-        powers = Power.query.all()
-        return render_template('powers.html', powers=powers)
-
-    @app.route('/heroes/<int:hero_id>')
-    def hero_details(hero_id):
-        hero = Hero.query.get(hero_id)
-        return render_template('hero_details.html', hero=hero)
-
-    @app.route('/powers/<int:power_id>')
-    def power_details(power_id):
-        power = Power.query.get(power_id)
-        return render_template('power_details.html', power=power)
-
-    app.run(debug=True)
+    app.run(port=5555)
--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,33 +1,31 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-
-class Power(db.Model):
-    __tablename__ = 'power'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    description = db.Column(db.Text)
-
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'  # Use your database URL
+db = SQLAlchemy(app)
 
 class Hero(db.Model):
     __tablename__ = 'hero'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    super_name = db.Column(db.String(255))
+    name = db.Column(db.String(100))
+    # Other Hero attributes
 
+class Power(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    # Other Power attributes
 
 class HeroPower(db.Model):
-    __tablename__ = 'hero_power'
-
     id = db.Column(db.Integer, primary_key=True)
     hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('power.id'))
-    strength = db.Column(db.String(10))
+    # Additional fields if needed
 
-    hero = db.relationship('Hero', backref=db.backref('hero_powers', lazy=True))
-    power = db.relationship('Power', backref=db.backref('power_heroes', lazy=True))
+    hero = db.relationship('Hero', backref='powers')
+    power = db.relationship('Power', backref='heroes')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
